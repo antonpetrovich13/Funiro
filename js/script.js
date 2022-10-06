4ac735af49106deded9ac0ce80c308b468ebfa75
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2022-10-06 23:06:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -187,6 +187,30 @@ if (document.documentElement.clientWidth < 768) {
 	})
 }
 
+// СЛАЙДЕР MAIN
+if (document.querySelector('.slider-main__body')) {
+	new Swiper('.slider-main__body', {
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1,
+		spaceBetween: 32,
+		watchOverflow: true,
+		speed: 800,
+		loop: true,
+		loopAdditionalSlides: 5,
+		preloadImages: false,
+		parallax: true,
+		pagination: {
+			el: '.controls-slider-main__dotts',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.slider-main .slider-arrow_next',
+			prevEl: '.slider-main .slider-arrow_prev',
+		}
+	});
+}
+
 
 // ВАЛИДАЦИЯ ФОРМЫ
 /*document.addEventListener('DOMContentLoaded', function () {
