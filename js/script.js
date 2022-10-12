27b4041a6d1f020a863e8b9a6be91b22c3577215
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2022-10-12 22:06:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -502,6 +502,91 @@ if (document.querySelector('.slider-rooms__body')) {
 	});
 }
 
+// СЛАЙДЕР TIPS
+if (document.querySelector('.slider-tips__body')) {
+	new Swiper('.slider-tips__body', {
+		observer: true,
+		observeParents: true,
+		slidesPerView: 3,
+		spaceBetween: 32,
+		watchOverflow: true,
+		speed: 800,
+		loop: true,
+		pagination: {
+			el: '.slider-tips__dotts',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.slider-tips .slider-arrow_next',
+			prevEl: '.slider-tips .slider-arrow_prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1.1,
+				spaceBetween: 15
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 32
+			},
+		}
+	});
+}
+
+// ГАЛЕРЕЯ Furniture
+const furniture = document.querySelector('.furniture__body');
+if (furniture && !isMobile.any()) {
+	const furnitureItems = document.querySelector('.furniture__items');
+	const furnitureColumn = Array.from(document.querySelector('.furniture__column'));
+
+	const speed = furniture.dataset.speed;
+
+	let positionX = 0;
+	let coordXprocent = 0;
+
+	function setMouseGalleryStyle() {
+		let furnitureItemsWidth = 0;
+		furnitureColumn.forEach(element => {
+			furnitureItemsWidth += element.offsetWidth;
+		});
+
+		const furnitureDifferent = furnitureItemsWidth - furniture.offsetWidth;
+		const distX = Math.floor(coordXprocent - positionX);
+
+		positionX = positionX + (distX * speed);
+		let position = furnitureDifferent / 200 * positionX;
+
+		furnitureItems.style.cssText = `transform: translate3d(${-position}px, 0, 0);`;
+
+		if (Math.abs(distX) > 0) {
+			requestAnimationFrame(setMouseGalleryStyle);
+		} else {
+			furniture.classList.remove('_init');
+		}
+	}
+
+	furniture.addEventListener('mousemove', function (e) {
+		const furnitureWidth = furniture.offsetWidth;
+
+		const coordX = e.pageX - furnitureWidth / 2;
+
+		coordXprocent = coordX / furnitureWidth * 27;
+
+		if (!furniture.classList.contains('_init')) {
+			requestAnimationFrame(setMouseGalleryStyle);
+			furniture.classList.add('_init');
+		}
+	})
+}
+
+
+
+
+
 
 // ВАЛИДАЦИЯ ФОРМЫ
 /*document.addEventListener('DOMContentLoaded', function () {
